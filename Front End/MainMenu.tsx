4dc0114512e1a6c2bdc9eedc85a31b5c1e592f41
Nicholas Keenan <nicholasekeenan@gmail.com>
2026-04-15 10:26:13 -0400

    MainMenu.tsx description header

---
+++ Front End/MainMenu.tsx
@@ -0,0 +1,6 @@
+//Main menu of the website, contains a popup menu that:
+//  - Welcomes the user
+//  - Allows the user to select a language
+//  - Allows the user to select a theme (light/dark)
+//  - Explains how to use the website
+//  - Displays references, credits, libraries used, etc.
