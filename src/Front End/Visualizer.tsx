15956348e67a32511f271ccd802af06cfbe08625
Nicholas Keenan <218298568+NickyKeenan@users.noreply.github.com>
2026-04-22 10:29:11 -0400

    got visualizer working with orbit rings, still needs some bug fixes

--- src/Front End/Visualizer.tsx
+++ src/Front End/Visualizer.tsx
@@ -10,41 +10,75 @@ import {planetsMoons} from "../Back End/PlanetData";
 const SCALE_RADIUS = 1e-6
 const SCALE_DISTANCE = 1e-9
 
-const Visualizer: React.FC = () => {
-    return (
-
-    <Canvas camera={{ position: [0, 30, 60] }}>
-    <ambientLight intensity={0.4} />
-    <pointLight position={[0, 0, 0]} intensity={2} />
-    <OrbitControls />
-
-    const kerbol = planetsMoons.find ( p => p.name === "Kerbol" );
-    const bodies = planetsMoons.filter ( p => p.name !== "Kerbol" );
-
-    {planetsMoons.map((body, i) => {
-
-        console.log("visualizer loaded");
-
-        const radius = body.planetRadius * SCALE_RADIUS;
-        const distance = body.semiMajorAxis * SCALE_DISTANCE;
-
-        console.log(body.name, body.semiMajorAxis);
+const OrbitRing = ( { radius }: { radius: number } ) => {
 
         return (
-            <mesh
-                key={body.id}
-                position={[distance, 0, 0]}
-            >
-            <sphereGeometry args={[radius, 32, 32]} />
-            <meshStandardMaterial
-                color={body.hasOxygen ? "cyan" : "gray"}
-            />
-            </mesh>
-        );
-    })}
-    </Canvas>
 
-    );
+            <mesh rotation = { [ -Math.PI / 2, 0, 0 ] } >
+
+                <ringGeometry args={[radius - 0.2, radius + 0.2, 64]} />
+                <meshBasicMaterial color="white" side={2} />
+
+            </mesh>
+
+        );
+
+    };
+
+const Visualizer: React.FC = () => {
+
+    const kerbol = planetsMoons.find( p => p.name === "Kerbol" );
+    const bodies = planetsMoons.filter( p => p.name !== "Kerbol" );
+
+    return (
+
+        <Canvas camera={{ position: [0, 200, 0], up: [0, 0, -1] }}>
+            <ambientLight intensity={0.4} />
+            <pointLight position={[0, 0, 0]} intensity={2} />
+            <OrbitControls maxPolarAngle = { Math.PI / 2 } minPolarAngle = { Math.PI / 2 } />
+
+            {planetsMoons.map((body, i) => {
+
+                console.log("visualizer loaded");
+
+                const radius = 1.5;
+                const distance = body.semiMajorAxis * SCALE_DISTANCE;
+
+                const angle = ( i / bodies.length ) * Math.PI * 2;
+                const x = Math.cos(angle) * distance;
+                const z = Math.sin(angle) * distance;
+
+                {kerbol && (
+
+                    <mesh position = { [ 0,0,0 ] }>
+
+                        <sphereGeometry args={[5, 32, 32]} />
+                        <meshBasicMaterial color = "yellow"/>
+
+                    </mesh>
+
+                )}
+
+                return (
+
+                    <React.Fragment key = { body.id }>
+
+                        <OrbitRing radius = { distance }/>
+
+                        <mesh position = { [ x, 0, z ] }>
+
+                            <sphereGeometry args={[radius, 16, 16]} />
+                            <meshStandardMaterial color = {body.hasOxygen ? "cyan" : "gray"} />
+
+                        </mesh>
+
+                    </React.Fragment>
+
+                );
+            })}
+        </Canvas>
+
+        );
 };
 
 export default Visualizer;
